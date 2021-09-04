aa18ade8786a15ebafbe98aea27c04976dea7dfb
Ashik0410 <hameedashik100@gmail.com>
2021-09-04 22:33:18 +0530

    update

---
+++ backend/Data/notes.js
@@ -0,0 +1,23 @@
+const notes = [
+    {
+        _id: "1",
+        title: "ASHik",
+        content: "hello world hello worldhello worldhello world"
+    },
+    {
+        _id: "2",
+        title: "ASHik",
+        content: "hello world hello worldhello worldhello world"
+    },
+    {
+        _id: "3",
+        title: "ASHik",
+        content: "hello world hello worldhello worldhello world"
+    }, {
+        _id: "4",
+        title: "ASHik",
+        content: "hello world hello worldhello worldhello world"
+    },
+];
+
+module.exports = notes;
